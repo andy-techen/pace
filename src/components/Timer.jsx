7f61d3519e12c61b23a945aed5b5b8ce3311de5f
Andy Chen <andy27479@gmail.com>
2022-04-11 22:31:25 -0400

    added base eclipse and container

---
+++ src/components/Timer.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import '../styles/Timer.css';
+
+const Timer = () => {
+    const [currTime, setCurrTime] = useState(0);
+
+    const updateTime = () => {
+
+    }
+
+    return (
+        <div className="timer-container">
+            <div className='timer-eclipse'></div>
+        </div>
+    );
+}
+
+export default Timer;
