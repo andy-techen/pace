2997eb9ae8a54f089981117c81bdd9533d2d2825
Andy Chen <andy27479@gmail.com>
2022-04-14 00:22:39 -0400

    set up states, handling functions pending

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -3,6 +3,8 @@ import '../styles/Timer.css';
 
 const Timer = () => {
     const [currTime, setCurrTime] = useState(0);
+    const [duration, setDuration] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
 
     const updateTime = () => {
 
@@ -11,6 +13,7 @@ const Timer = () => {
     return (
         <div className="timer-container">
             <div className='timer-eclipse'></div>
+            <div className='timer-inner'></div>
         </div>
     );
 }
