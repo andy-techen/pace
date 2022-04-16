4d2027880ba6b068e3bfb717f9f5cde3f4d5b767
Andy Chen <andy27479@gmail.com>
2022-04-16 14:33:45 -0400

    added states, notification

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Timer from './components/Timer';
 import TimeInput from './components/TimeInput';
 import AudioButtonGroup from './components/AudioButtonGroup';
+import Notification from './components/Notification';
 import './styles/App.css';
 
 function App() {
+  const [duration, setDuration] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [complete, setComplete] = useState(false);
+
   return (
     <>
       <Header />
-      <Timer />
-      <TimeInput />
-      <AudioButtonGroup />
+      <Notification complete={complete} />
+      <Timer duration={duration} setIsRunning={setIsRunning} setComplete={setComplete} />
+      <TimeInput setDuration={setDuration} />
+      <AudioButtonGroup duration={duration} isRunning={isRunning} />
     </>
   );
 }
