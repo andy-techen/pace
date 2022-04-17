388b112e8be543f1490b866793e89a6f92f91e44
Andy Chen <andy27479@gmail.com>
2022-04-17 14:23:20 -0400

    initial commit, subpages

---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import Header from './Header';
+
+const Profile = () => {
+    return (
+        <>
+            <p>This is the profile</p>
+        </>
+    );
+}
+
+export default Profile;
---
+++ src/components/Stats.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const Stats = () => {
+    return (
+        <>
+        </>
+    );
+}
+ 
+export default Stats;
