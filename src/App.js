33aee28827549e2f9c9d6e82a243192ab750a8f4
Andy Chen <andy27479@gmail.com>
2022-04-19 23:10:47 -0400

    init userid in localstorage, removed stats

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { v4 as uuidv4 } from 'uuid';
 import Main from "./components/Main";
 import Profile from "./components/Profile";
-import Stats from "./components/Stats";
 import Header from "./components/Header";
 
 export default function App() {
+  useEffect(() => {
+    if (localStorage.getItem("userId") == null) {
+      localStorage.setItem("userId", uuidv4());  // set new userId for new session
+    }
+  }, []);
+
   return (
     <>
       <Header />
@@ -12,9 +19,6 @@ export default function App() {
         <Switch>
           <Route path="/profile">
             <Profile />
-          </Route>
-          <Route path="/stats">
-            <Stats />
           </Route>
           <Route path="/">
             <Main />
